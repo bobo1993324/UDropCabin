cff1df12a89b996e181d112318993ff8f37ec438
Boren Zhang <bobo1993324@gmail.com>
2014-06-21 00:59:38 -0700

    fix package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir package
-cp build/udropcabin build/qml uDropCabin-content.json manifest.json uDropCabin.desktop uDropCabin.json UDropCabin.png package -rf
+cp build/udropcabin build/qml uDropCabin-content.json manifest.json uDropCabin.desktop uDropCabin.json logo.png package -rf
 cd package
 click build .
 cp *.click ..
