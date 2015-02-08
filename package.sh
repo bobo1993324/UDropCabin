4557f24846b0b1f0a2b5d9a9d31efa1c517371bb
Boren Zhang <bobo1993324@gmail.com>
2015-02-08 04:15:05 -0500

    onlineAccount first try

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir package
-cp build/udropcabin build/qml uDropCabin-content.json manifest.json uDropCabin.desktop uDropCabin.json logo.png package -rf
+cp build/udropcabin build/qml uDropCabin-content.json manifest.json uDropCabin.desktop uDropCabin.json logo.png dropbox.provider dropbox-online-account-plugin dropbox.png package -rf
 cd package
 click build .
 cp *.click ..
